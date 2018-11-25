2686c968f18f67d7e3b61d58ab31b987feab86b0
petitviolet <violethero0820@gmail.com>
2018-11-25 21:29:53 +0900

    updated bench mark code

--- modules/main/src/main/scala/net/petitviolet/example/ForBench.scala
+++ modules/main/src/main/scala/net/petitviolet/example/ForBench.scala
@@ -2,18 +2,22 @@ package net.petitviolet.example
 
 import org.openjdk.jmh.annotations._
 
-import scala.util.Random
-
 @State(Scope.Thread)
 class ForBench {
-  private lazy val a = Random.nextInt()
-  private lazy val b = Random.nextInt()
+  private val sentence = "In 2017 I would like to run ALL languages in one VM."
+  private val answer = 7
 
   @Benchmark
   @BenchmarkMode(Array(Mode.Throughput))
-  def bench_add() = {
-    add(a, b)
+  def bench_upperCaseCount() = {
+    upperCaseCount(sentence)
   }
 
-  private def add(i: Int, j: Int): Int = i + j
+  private def upperCaseCount(args: String) = {
+    val start = System.nanoTime()
+    val sentence = String.join(" ", args)
+    require(sentence.filter(Character.isUpperCase).length == answer)
+    val end = System.nanoTime()
+    end - start
+  }
 }
